5b60006334c1749b168a62dd5e563868d5c97caf
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-01-06 23:05:58 +0900

    add interceptor

--- Product/src/main/java/com/develop/ProductsService/ProductsServiceApplication.java
+++ Product/src/main/java/com/develop/ProductsService/ProductsServiceApplication.java
@@ -1,15 +1,23 @@
 package com.develop.ProductsService;
 
+import com.develop.ProductsService.commnad.interceptors.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProductsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsServiceApplication.class, args);
+    }
 
+    @Autowired
+    public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus) {
+        commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+    }
 }
