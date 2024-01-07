1c349b03e66519c1bbcfd14ed01c7c9ffea90bd9
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-01-07 23:37:44 +0900

    add initial OrdersSaga

---
+++ OrdersService/src/main/java/com/development/OrdersService/saga/OrderSaga.java
@@ -0,0 +1,21 @@
+package com.development.OrdersService.saga;
+
+import com.development.OrdersService.core.events.OrderCreatedEvent;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.spring.stereotype.Saga;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Saga
+public class OrderSaga {
+
+    @Autowired
+    private transient CommandGateway commandGateway;
+
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent event) {
+
+    }
+}
