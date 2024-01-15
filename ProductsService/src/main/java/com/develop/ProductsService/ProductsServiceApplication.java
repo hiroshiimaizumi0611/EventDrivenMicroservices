aaeb24770acb5062c45594677bccddf9c15ebdb7
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-01-16 00:01:31 +0900

    add snapshot

--- ProductsService/src/main/java/com/develop/ProductsService/ProductsServiceApplication.java
+++ ProductsService/src/main/java/com/develop/ProductsService/ProductsServiceApplication.java
@@ -4,16 +4,20 @@ import com.develop.ProductsService.commnad.interceptors.CreateProductCommandInte
 import com.develop.ProductsService.core.errorhandling.ProductsServiceEventsErrorHandler;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@Import({ AxonXStreamConfig.class })
+@Import({AxonXStreamConfig.class})
 public class ProductsServiceApplication {
 
     public static void main(String[] args) {
@@ -29,5 +33,10 @@ public class ProductsServiceApplication {
     public void configure(EventProcessingConfigurer config) {
         config.registerListenerInvocationErrorHandler("product-group",
                 conf -> new ProductsServiceEventsErrorHandler());
+    }
+
+    @Bean(name="productSnapshotTriggerDefinition")
+    public SnapshotTriggerDefinition productSnapshotTriggerDefinition(Snapshotter snapshotter) {
+        return new EventCountSnapshotTriggerDefinition(snapshotter, 3);
     }
 }
